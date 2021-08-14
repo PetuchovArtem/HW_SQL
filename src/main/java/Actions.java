5d2c67f9866b6a38a0dacbebb72a74e0cb3c96e1
PetuchovArtem <petuchov.artem@gmail.com>
2021-08-14 13:19:03 +0300

    Create ReplenishmentAccount, getAccountBalance Actions

--- src/main/java/Actions.java
+++ src/main/java/Actions.java
@@ -1,3 +1,4 @@
+import javax.sql.rowset.JdbcRowSet;
 import java.sql.*;
 
 public class Actions {
@@ -49,7 +50,9 @@ public class Actions {
         }
     }
 
-    public void ReplenishmentAccount(int AccountIdSet, int userIdSet, int balanceSet, String currencySet) {
+    public void ReplenishmentAccount(int TransactionIdSet, int accountIdSet, int amountSet) {
+        int newBalance = 0;
+        int currentBalance = 0;
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -59,17 +62,53 @@ public class Actions {
         try (Connection connect = DriverManager.getConnection(URL, LOGIN, PASS);
              Statement statement = connect.createStatement();) {
             try {
-                String sql = "INSERT INTO accounts(accountid, userid, balance, currency) VALUES (" + AccountIdSet + " , " + userIdSet + " , " + balanceSet + " , " + "\'" + currencySet + "\'" + ")";
-                statement.executeUpdate(sql);
-                System.out.println("Аккаунт для пользователя id=" + userIdSet + " создан");
+            String sql = "INSERT INTO transactions (transactinid, accountid, amount) VALUES (" + TransactionIdSet + " , " + accountIdSet + " , " + amountSet + ")";
+            statement.executeUpdate(sql);
+            String sql2 = "SELECT balance FROM accounts WHERE accountid =" + accountIdSet;
+            ResultSet rs = statement.executeQuery(sql2);
+
+            while (rs.next()) {
+                currentBalance = Integer.parseInt(
+                        String.valueOf(rs.getInt("balance")));
+            }
+            newBalance = currentBalance + amountSet;
+            String sql3 = "UPDATE accounts SET balance =" + newBalance + " WHERE accountid=" + accountIdSet;
+            statement.executeUpdate(sql3);
+
+                System.out.println("Аккаунт id=" + accountIdSet + " пополнен");
             } catch (Exception e) {
-                System.out.println("Ошибка создания аккаунта,проверьте правильность данных");
+                System.out.println("Ошибка пополнения аккаунта,проверьте правильность данных");
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
+
+    public void getAccountBalance(int accountIdSet) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Oshibka drivera");
+        }
+
+        try (Connection connect = DriverManager.getConnection(URL, LOGIN, PASS);
+             Statement statement = connect.createStatement();) {
+            try {
+                String sql = "SELECT AVG(balance) AS BALANCE FROM accounts WHERE accountid=" + accountIdSet;
+                ResultSet request2 = statement.executeQuery(sql);
+
+                while (request2.next()) {
+                    System.out.println("Балланс аккаунта " + request2.getDouble("BALANCE"));
+                }
+            } catch (Exception e) {
+                System.out.println("Ошибка получения балланса аккаунта,проверьте правильность данных");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
 
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,18 @@ public class Main {
     public static void main(String[] args) {
         Actions actions = new Actions();
         System.out.println("Создание клиента");
-        actions.Registred(2, "Alena", "Minsk");
+        actions.Registred(1, "Artiom", "Minsk");
         System.out.println("-----------------");
         System.out.println("Создание аккаунта");
-        actions.AddAccount(1, 2, 10000, "BYN");
-
+        actions.AddAccount(1, 1, 10000, "BYN");
+        System.out.println("-----------------");
+        System.out.println("Балланс аккаунта");
+        actions.getAccountBalance(1);
+        System.out.println("-----------------");
+        System.out.println("Пополнение аккаунта");
+        actions.ReplenishmentAccount(1,1,1000);
+        System.out.println("-----------------");
+        System.out.println("Балланс аккаунта");
+        actions.getAccountBalance(1);
     }
 }
