a840ead1eee98daf07f4700f3e4f2ef186e00c28
PetuchovArtem <petuchov.artem@gmail.com>
2021-08-14 12:13:53 +0300

    Create AddAccount Action

--- src/main/java/Actions.java
+++ src/main/java/Actions.java
@@ -15,14 +15,61 @@ public class Actions {
 
         try (Connection connect = DriverManager.getConnection(URL, LOGIN, PASS);
              Statement statement = connect.createStatement();) {
-
-            String sql = "INSERT INTO users(userid,name,address) VALUES (" +  userIdSet  + " , " + "\'" + nameSet + "\'" + " , " + "\'" + addressSet + "\'" + ")";
-
-            statement.executeUpdate(sql);
-
+            try {
+                String sql = "INSERT INTO users(userid,name,address) VALUES (" + userIdSet + " , " + "\'" + nameSet + "\'" + " , " + "\'" + addressSet + "\'" + ")";
+                statement.executeUpdate(sql);
+                System.out.println("Пользовтаель создан");
+            } catch (Exception e) {
+                System.out.println("Ошибка создания пользователя,проверьте правильность данных");
+            }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
     }
+
+    public void AddAccount(int AccountIdSet, int userIdSet, int balanceSet, String currencySet) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Oshibka drivera");
+        }
+
+        try (Connection connect = DriverManager.getConnection(URL, LOGIN, PASS);
+             Statement statement = connect.createStatement();) {
+            try {
+                String sql = "INSERT INTO accounts(accountid, userid, balance, currency) VALUES (" + AccountIdSet + " , " + userIdSet + " , " + balanceSet + " , " + "\'" + currencySet + "\'" + ")";
+                statement.executeUpdate(sql);
+                System.out.println("Аккаунт для пользователя id=" + userIdSet + " создан");
+            } catch (Exception e) {
+                System.out.println("Ошибка создания аккаунта,проверьте правильность данных");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public void ReplenishmentAccount(int AccountIdSet, int userIdSet, int balanceSet, String currencySet) {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Oshibka drivera");
+        }
+
+        try (Connection connect = DriverManager.getConnection(URL, LOGIN, PASS);
+             Statement statement = connect.createStatement();) {
+            try {
+                String sql = "INSERT INTO accounts(accountid, userid, balance, currency) VALUES (" + AccountIdSet + " , " + userIdSet + " , " + balanceSet + " , " + "\'" + currencySet + "\'" + ")";
+                statement.executeUpdate(sql);
+                System.out.println("Аккаунт для пользователя id=" + userIdSet + " создан");
+            } catch (Exception e) {
+                System.out.println("Ошибка создания аккаунта,проверьте правильность данных");
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Actions actions = new Actions();
-        actions.Registred(3, "Alena", "Minsk");
+        System.out.println("Создание клиента");
+        actions.Registred(2, "Alena", "Minsk");
+        System.out.println("-----------------");
+        System.out.println("Создание аккаунта");
+        actions.AddAccount(1, 2, 10000, "BYN");
 
     }
 }
